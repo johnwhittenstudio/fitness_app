3ad36420619aa5bd5f96fd8f8068691196fa32f9
John Whitten <johnwhitten.studio@gmail.com>
2023-04-28 13:30:38 -0700

    resolved ExerciseDB API bug

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,8 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 
+import { exerciseOptions, fetchData } from '../utils/fetchData';
+
 
 const SearchExercises = () => {
+    const [search, setSearch] = useState('')
+
+    const handleSearch = async () => {
+        if(search) {
+            const exercisesData = await fetchData
+            ('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
+
+            console.log(exercisesData);
+        }
+    }
+
   return (
     <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
         <Typography 
@@ -22,14 +35,31 @@ const SearchExercises = () => {
                         border: 'none', 
                         borderRadius: '4px'
                     },
-                    width: { lg: '1170px', xs: '350px'}
+                    width: { lg: '800px', xs: '350px'},
+                    backgroundColor: '#fff',
+                    borderRadius: '40px'
                 }}
                 height="76px"
-                value=""
-                onChange={(e) => {}}
+                value={search}
+                onChange={(e) => setSearch(e.target.value.toLowerCase()) }
                 placeholder="Search Exercises"
                 type="text"
             />
+            <Button className="search-btn"
+                sx={{
+                    bgcolor: '#FF2625',
+                    color: '#fff',
+                    textTransform: 'none',
+                    width: { lg: '175px', xs: '80px' },
+                    fontSize: { lg: '20px', xs: '14px' },
+                    height: '56px',
+                    position: 'absolute',
+                    right: '0'
+                }}
+                onClick={handleSearch}
+                >
+                Search
+            </Button>
         </Box>
     </Stack>
   )

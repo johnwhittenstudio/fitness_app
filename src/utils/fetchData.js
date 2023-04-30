3099c015352d31130ece41c57ed3830cadaa16e2
John Whitten <johnwhitten.studio@gmail.com>
2023-04-29 18:59:51 -0700

    complete ExerciseDetail and ExerciseVideos

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -7,6 +7,16 @@ export const exerciseOptions = {
     },
   };
 
+export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+      'X-RapidAPI-Key': '61e8330247msh6c9d7770178733cp154fafjsn1aa72534d5c4',
+      // 'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    }
+  };
+
+
 export const fetchData = async (url, options) => {
     const response = await fetch(url, options);
     const data = await response.json();
